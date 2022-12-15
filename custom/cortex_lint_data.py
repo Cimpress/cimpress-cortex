6c14a110a88d2aef60e6530899f16003a97d59e5
vrunali398 <52998940+vrunali398@users.noreply.github.com>
2022-12-15 13:33:02 +0530

    Add linting code

---
+++ custom/cortex_lint_data.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python
+
+import json
+import requests
+import os
+
+service_name = os.environ.get('SERVICE_NAME')
+output = json.load(open('report/api_lint_result.json'))
+data = {}
+data['serviceName'] = service_name
+data['key'] = 'spectral'
+data['output'] = output
+lintdump = json.dumps(data)
+lint_file = json.loads(lintdump)
+print (lintdump)
+
+url = "https://api.getcortexapp.com/api/v1/custom-integrations/data/f1492006-6383-453a-8313-05757585fa12"
+headers = {
+            'content-type': 'application/json'
+        }
+
+lint = requests.post(url, json=lint_file, headers=headers)
+print("Pushed lint data")
+print(lint.text)
